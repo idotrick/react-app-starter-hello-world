8c55b7c4093888094bd1a3b8a6b25bdcdb0ea619
Damith <dwanninayaka@gmail.com>
2019-08-14 10:05:41 +0800

    Original

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This project is just a starter project for react.js. It will just print "Hello World" on the screen.
 
 ## Available Scripts
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import reactDom from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const view = <h1>Hello World</h1>;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+reactDom.render(view, document.getElementById("root"));
